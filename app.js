cf51ddf7ab4fb5ee07c5272e80c81c494b906982
kwmorlock <kmorlockcodes@gmail.com>
2021-02-14 15:03:06 -0500

    set: app.js morgan

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const connectDB = require('./config/db')
+const morgan = require("morgan");
+const connectDB = require("./config/db");
 
 dotenv.config({ path: "./config/config.env" });
 
-connectDB()
+connectDB();
 
 const app = express();
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 
 const PORT = process.env.PORT || 3000;
 
