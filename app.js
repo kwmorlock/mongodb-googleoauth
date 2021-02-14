25c2536c8525a8e430b4627d485100e9ca1752c1
kwmorlock <kmorlockcodes@gmail.com>
2021-02-14 15:10:11 -0500

    set: app.js handlebars

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
 const connectDB = require("./config/db");
+const exphbs = require("express-handlebars");
 
 dotenv.config({ path: "./config/config.env" });
 
@@ -12,6 +13,32 @@ const app = express();
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
+
+// Handlebars Helpers
+const {
+  formatDate,
+  stripTags,
+  truncate,
+  editIcon,
+  select,
+} = require("./helpers/hbs");
+
+// Handlebars
+app.engine(
+  ".hbs",
+  exphbs({
+    helpers: {
+      formatDate,
+      stripTags,
+      truncate,
+      editIcon,
+      select,
+    },
+    defaultLayout: "main",
+    extname: ".hbs",
+  })
+);
+app.set("view engine", ".hbs");
 
 const PORT = process.env.PORT || 3000;
 
