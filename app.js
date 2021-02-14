27db93624279ea997c5141cbafdeec3270cee790
kwmorlock <kmorlockcodes@gmail.com>
2021-02-14 14:31:41 -0500

    set: app.js port

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
-const express = require('express')
-const dotenv = require('dotenv')
+const express = require("express");
+const dotenv = require("dotenv");
 
-dotenv.config({ path: './config/config.env' })
+dotenv.config({ path: "./config/config.env" });
 
-const app = express()
+const app = express();
 
-app.listen()
+const PORT = process.env.PORT || 3000;
+
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
